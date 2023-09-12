799ee427d8a5707edcf18f3c65af8d03a4aee476
emgreye <JakeRMcAuliffe@gmail.com>
2023-09-12 13:54:48 +0800

    doesn't get stuck in same location

--- main.py
+++ main.py
@@ -11,23 +11,38 @@ env = JoypadSpace(env, SIMPLE_MOVEMENT)
 done = True
 env.reset()
 point = 0
-death_points = []
+jump_points = []
 stored_life = 3
+stored_loc = 0
 for step in range(5000):
     #action = env.action_space.sample()
-    if (point in death_points):
+    if (point in jump_points):
         action = 2
+        #jump if this area previously caused mario to die
     else:
         action = 1
     obs, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
     if (stored_life > info['life']):
+        # detects if mario has died
         stored_life = info['life']
         if (point>20):
             for i in range(point-20, point):
-                death_points.append(i)
+                jump_points.append(i)
+                # adds previous 20 points into the jump points array
         point = 0
+    if (step % 20 == 0):
+        # every 20 steps, check if mario has changed position
+        if (point > 20):
+            if (info['x_pos'] == stored_loc):
+                for i in range(point-20, point):
+                    jump_points.append(i)
+                state = env.reset()
+                point = 0
+                # reset the environment if mario stops moving
+        stored_loc = info['x_pos']
     if done:
         state = env.reset()
+        point = 0
     point += 1
 env.close()
